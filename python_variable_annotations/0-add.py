d91880fa805ed844fa4fb8363e4081ffc2862e0b
Abdulhammed <a.abdulazee@alustudent.com>
2024-09-06 12:10:05 +0200

    docstring updated

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 
-# def add(a: float, b: float) -> float:
-#     """"""
-#     return a + b
-
 def add(a: float, b: float) -> float:
-    """Adds two numbers."""
+    """
+    Adds two numbers
+    :param a: a float number
+    :param b: a float number
+    :return: returns a float
+    """
     return a + b
