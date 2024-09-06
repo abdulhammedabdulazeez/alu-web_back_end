3bb6605258a7e60038f511b7d81ae88cf45603d0
Abdulhammed <a.abdulazee@alustudent.com>
2024-09-06 15:16:50 +0200

    to_string function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a given float number.
+
+    Args:
+    n (float): The input float number.
+
+    Returns:
+    str: The string representation of the input float number.
+    """
+    return str(n)
