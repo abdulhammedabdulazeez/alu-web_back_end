82fc968976c8f1fb89bf9ce9460ee75b5bca3fc9
Abdulhammed <a.abdulazee@alustudent.com>
2024-09-06 15:13:29 +0200

    floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a given float number.
+
+    Args:
+    n (float): The input float number.
+
+    Returns:
+    int: The floor of the input float number.
+    """
+    return math.floor(n)
