ec0672aa1c2e7f34129c5c730ec410ea19cd4aaa
Abdulhammed <a.abdulazee@alustudent.com>
2024-09-06 15:24:53 +0200

    list annotation

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Returns the sum of a list of floats.
+
+    Args:
+    input_list (list[float]): The input list of floats.
+
+    Returns:
+    float: The sum of the input list of floats.
+    """
+    return sum(input_list)
