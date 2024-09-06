5f8dda3566e5976d11b971c375e625c508c6c067
Abdulhammed <a.abdulazee@alustudent.com>
2024-09-06 16:45:49 +0200

    multiplier function

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by the input multiplier.
+
+    Args:
+    multiplier (float): The input multiplier.
+
+    Returns:
+    Callable[[float], float]: A function that takes a float and returns the product of the input float and the multiplier.
+    """
+    def multiply(x: float) -> float:
+        return x * multiplier
+    return multiply
