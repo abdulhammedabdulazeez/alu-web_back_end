b1810d7998af4dadc5b4e17efb54bc5a57af8ccd
Abdulhammed <a.abdulazee@alustudent.com>
2024-09-06 16:18:53 +0200

    Mixed annotation

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# from typing import Union, Tuple
+
+
+def to_kv(k: str, v: int | float) -> tuple[str, float]:
+    """
+    Returns a tuple containing the input string k and the square of the input int/float v.
+
+    Args:
+    k (str): The input string.
+    v (Union[int, float]): The input int or float.
+
+    Returns:
+    Tuple[str, float]: A tuple containing the input string k and the square of v as a float.
+    """
+    return k, float(v) ** 2
