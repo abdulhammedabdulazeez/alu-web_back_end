ac1792c289a43a01fc7a0d5c1258bc95c9700371
Abdulhammed <a.abdulazee@alustudent.com>
2024-09-06 14:50:21 +0200

    string function

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 def concat(str1: str, str2: str) -> str:
     """
     Concatenates two strings provided as parameters to the function
-    :param str1: a `string`
+    :param str1: takes a `string`
     :param str2: a `string`
     :return: returns a string
     """
